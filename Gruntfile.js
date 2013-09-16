a43c4da958a38335721d112365b2cde78b8924f0
WebCoreIT <info@webcore-it.com>
2013-09-16 16:30:44 +0200

    Fixed Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,6 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		// ----------------------------------------------------------------------
 		jshint: {
-			options: {
-				jshintrc: '.jshintrc'
-			},
 			gruntfile: {
 				src: 'Gruntfile.js'
 			},
