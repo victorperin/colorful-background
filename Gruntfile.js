6fc649c5378d8aa87d16e8a4584b5495ee6beec6
WebCoreIT <info@webcore-it.com>
2013-09-16 16:23:45 +0200

    Website moved

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		// ----------------------------------------------------------------------
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			website: {
+				src: ['src/js/*.js']
+			},
+		},
+		// ----------------------------------------------------------------------
+		concat: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+				stripBanners: true
+			},
+			website_js: {
+				src: ['src/js/*.js'],
+				dest: 'src/js/gen/script-all.js'
+			},
+			website_css: {
+				src: ['src/css/*.css'],
+				dest: 'src/css/gen/style-all.css'
+			}
+		},
+		// ----------------------------------------------------------------------
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			website: {
+				src: 'src/js/gen/script-all.js',
+				dest: 'js/script.min.js'
+			}
+		},
+		// ----------------------------------------------------------------------
+		cssmin: {
+			options: {
+				banner: '<%= banner %>',
+			},
+			website: {
+				src: ['src/css/gen/style-all.css'],
+				dest: 'css/style.min.css'
+			}
+		},
+		// ----------------------------------------------------------------------
+		htmlmin: {
+			website: {
+				options: {                                 
+				removeComments: true,
+				collapseWhitespace: true
+			},
+			files:{
+				'index.html':'src/index.html'
+				}
+			}
+		}
+	});
+
+	
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	
+
+
+	// Default task(s).
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin', 'htmlmin']);
+
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+# colorful-background
+
+A Colorful CSS Gradient Background Generator. 
+[Here is the first preview](https://github.com/webcore-it/colorful-background/website/index.html)
+
+
+## Pre-Release (Not ready for usage)
+
+This is a pre-release version of Colorful Background.
+
+
+## License
+Copyright (c) 2013 Web Core IT
+Licensed under the MIT license.
