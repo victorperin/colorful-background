50793df710f4b84bc39e751870fcaafe56f9ad5d
WebCoreIT <info@webcore-it.com>
2013-09-16 16:13:01 +0200

    Moved Website in other branch.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,10 +16,7 @@ module.exports = function(grunt) {
 					jshintrc: 'src/.jshintrc'
 				},
 				src: ['src/**/*.js']
-			},
-			website: {
-				src: ['website-src/js/*.js']
-			},
+			}
 		},
 		// ----------------------------------------------------------------------
 		concat: {
@@ -30,14 +27,6 @@ module.exports = function(grunt) {
 			src: {
 				src: ['src/**/*.js'],
 				dest: '<%= pkg.name %>.js'
-			},
-			website_js: {
-				src: ['website-src/js/*.js'],
-				dest: 'website-src/js/gen/script-all.js'
-			},
-			website_css: {
-				src: ['website-src/css/*.css'],
-				dest: 'website-src/css/gen/style-all.css'
 			}
 		},
 		// ----------------------------------------------------------------------
@@ -48,54 +37,18 @@ module.exports = function(grunt) {
 			src: {
 				src: '<%= pkg.name %>.js',
 				dest: '<%= pkg.name %>.min.js'
-			},
-			website: {
-				src: 'website-src/js/gen/script-all.js',
-				dest: 'website/js/script.min.js'
 			}
-		},
-		// ----------------------------------------------------------------------
-		cssmin: {
-			options: {
-				banner: '<%= banner %>',
-			},
-			website: {
-				src: ['website-src/css/gen/style-all.css'],
-				dest: 'website/css/style.min.css'
-			}
-		},
-		// ----------------------------------------------------------------------
-		htmlmin: {
-			website: {
-				options: {                                 
-				removeComments: true,
-				collapseWhitespace: true
-			},
-			files:{
-				'website/index.html':'website-src/index.html'
-				}
-			}
-		},
-		// ----------------------------------------------------------------------
-		copy: {
-			website: {
-				src: '<%= pkg.name %>.min.js',
-				dest: 'website/<%= pkg.name %>.min.js',
-			},
-		},
+		}
 
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-htmlmin');
-	grunt.loadNpmTasks('grunt-contrib-copy');
 	
 
 
 	// Default task(s).
-	grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin', 'htmlmin', 'copy']);
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 
 };
